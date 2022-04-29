e972343a8d74b9f061260901670597ec87c86f05
Jonas Ferreira <suporte@yesflex.com.br>
2022-04-29 12:31:34 -0300

    first commit

---
+++ aula6/index.js
@@ -0,0 +1,17 @@
+let nome; // Declarou a variável
+
+nome = "João"; // Inicializando a variável
+
+console.log(nome, "nasceu em 1984");
+console.log("Em 200 ", nome, " conheceu Maria.");
+
+nome = "Jonas";
+console.log(nome, " casou-se com maria em 2012");
+console.log("maria Teve 1 filho com ", nome, " em 2015");
+console.log("O Filho de ", nome, " se chama Eduardo");
+
+/*
+Não podemos criar variáveis com palavras reservadas
+let if
+let let
+*/
